0a0c8601f57c16eb2aa16971ce4c4538862f359b
mhupfauer <mhupfauer@users.noreply.github.com>
2021-08-05 19:45:23 +0200

    Update scantofile.sh

--- images/brother_scan/scantofile.sh
+++ images/brother_scan/scantofile.sh
@@ -6,7 +6,7 @@ NAME=scan_$(date "+%d.%m.%Y-%H:%M:%S").tiff
 
 whoami
 
-scanimage -L >> /var/log/brother/scan_scr.log
+scanimage -L
 scanimage --device-name="brother4:net1;dev0" --resolution 200 --format=png --batch=$TMP"/out-%d.png" -x 210 -y 297
 
 cd $TMP
